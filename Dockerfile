7c173ac39acb7ab1f4bd5878065b45df107a4484
Quierati <julio.quierati@gmail.com>
2017-08-22 13:10:58 -0300

    teste v2

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.6-slim
+
+LABEL maintainer="julio.quierati@gmail.com" version="1.0" author="Julio Quierati"
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+EXPOSE 8000
+ADD . /opt/app
+WORKDIR /opt/app
+
+CMD ["python3", "application.py" ]
--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 import logging
 import logging.handlers
+import os
+import pathlib
 
 from wsgiref.simple_server import make_server
 
@@ -9,7 +11,10 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # Handler
-LOG_FILE = '/opt/python/log/sample-app.log'
+LOG_DIR = './log'
+pathlib.Path(LOG_DIR).mkdir(parents=True, exist_ok=True) 
+
+LOG_FILE = LOG_DIR + '/sample-app.log'
 handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
 handler.setLevel(logging.INFO)
 
@@ -55,6 +60,7 @@ welcome = """
     -webkit-transition-property: text-shadow;
     -webkit-transition-duration: 4s;
     text-shadow: #fff 0px 0px 25px;
+
   }
   a {
     color: #0188cc;
